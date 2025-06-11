8bfe8387f1fe6bf1f5c44d57a13d6b54317c9c07
yoonhaw <bbabrehamza@gmail.com>
2025-06-11 19:07:41 +0100

    done

--- school_management/models.py
+++ school_management/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models.signals import post_delete, pre_save
+from django.utils import timezone
 from django.dispatch import receiver
 
 class UserProfile(models.Model):
